9757d470ff1ea092ddb3247b15dc10ece317c680
Lucas Castellani <lucas100castellani@gmail.com>
2022-06-29 22:59:45 -0300

    fix

--- Config.php
+++ Config.php
@@ -7,6 +7,6 @@ const URL_BASE = "http://www.localhost/Rotas";
 // constantes do banco
 const DB_DRIVER = "mysql";
 const DB_HOST   = "localhost";
-const DB_DBNAME = "hote";
+const DB_DBNAME = "hotel";
 const DB_USER   = "root";
 const DB_PASS   = "";
