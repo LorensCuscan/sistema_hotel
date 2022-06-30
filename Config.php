1f3780758105482fe7a37e46b163fe12738a1b5b
Lucas Castellani <lucas100castellani@gmail.com>
2022-06-29 22:59:24 -0300

    constantes para banco

--- Config.php
+++ Config.php
@@ -3,3 +3,10 @@
 // constantes do projeto
 const APP_NAME = "sistema_hotel";
 const URL_BASE = "http://www.localhost/Rotas";
+
+// constantes do banco
+const DB_DRIVER = "mysql";
+const DB_HOST   = "localhost";
+const DB_DBNAME = "hote";
+const DB_USER   = "root";
+const DB_PASS   = "";
