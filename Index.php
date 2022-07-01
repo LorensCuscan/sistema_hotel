b81a3a30da4c268f92b3ca3ab0d6d86fbda778f5
Lucas Castellani <lucas100castellani@gmail.com>
2022-06-30 21:07:05 -0300

    ajustes

--- Index.php
+++ Index.php
@@ -3,6 +3,7 @@
 require __DIR__ . "/vendor/autoload.php";
 require __DIR__ . "/config.php";
 require __DIR__ . "/database.php";
+require __DIR__ . "/helpers.php";
 
 use CoffeeCode\Router\Router;
 
---
+++ app/Controller/AdminController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view('site/index');
+    }
+}
