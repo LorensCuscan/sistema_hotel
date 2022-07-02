3fb2abd2e9e55d60cce5a6bf832192f9b678558f
Lucas Castellani <lucas100castellani@gmail.com>
2022-07-02 10:46:07 -0300

    com sertado

--- view/site/index.php
+++ view/site/index.php
@@ -1,12 +1,15 @@
 <?= view('components/header'); ?>
 
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
+
   <div class="carousel-inner">
+
     <div class="carousel-item active ">
       <img src="https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2018/10/19/1052/Grand-Hyatt-Sao-Paulo-P843-Grand-Twin-Bed-Room.jpg/Grand-Hyatt-Sao-Paulo-P843-Grand-Twin-Bed-Room.16x9.jpg?imwidth=1280" style="width:100vw; max-height:90vh;" class="d-block mh-75 h-75" alt="...">
       <div class="carousel-caption d-none d-md-block">
@@ -14,6 +17,7 @@
         <p>Pensados em cada detalhe pra voce!</p>
       </div>
     </div>
+
     <div class="carousel-item">
       <img src="https://www.oitihotel.com.br/wp-content/uploads/2021/10/04.jpeg" style="width:100vw; max-height:90vh;" class="d-block" alt="...">
       <div class="carousel-caption d-none d-md-block">
@@ -21,6 +25,7 @@
         <p>Para tornar a sua experiencia única.</p>
       </div>
     </div>
+
     <div class="carousel-item">
       <img src="https://pizarromoveis.com.br/wp-content/uploads/2019/08/quartos-de-hotel-feitos-com-moveis-planejados-1280x720.png"  style="width:100vw; max-height:90vh;" class=" d-block" alt="...">
       <div class="carousel-caption d-none d-md-block">
@@ -28,16 +33,20 @@
         <p>o hotel Atrium fica há apenas 5 minutos da praia.</p>
       </div>
     </div>
+
   </div>
-</div>
-<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+  
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"   data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
-</button>
-<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-  <span class="visually-hidden">Next</span>
-</button>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+
+</div>
+
 
 
 <section>
