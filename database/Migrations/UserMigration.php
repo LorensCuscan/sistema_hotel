3cf7b6ce7a7259bfb727b25b7f6e090f95d0dcac
Lucas Castellani <lucas100castellani@gmail.com>
2022-07-01 21:40:18 -0300

    correcao tabela usuarios

--- database/Migrations/UserMigration.php
+++ database/Migrations/UserMigration.php
@@ -3,15 +3,19 @@
 namespace Database\Migrations;
 
 use Illuminate\Database\Migrations\Migration;
-
-class UserMigration extends Migration
+class UserMigration extends RunMigration
 {
-    public function up()
+    public function __construct($capsule)
     {
-        Capsule::schema()->create('users', function ($table)
+        $capsule::schema()->dropIfExists('users');
+
+        $capsule::schema()->create('users', function ($table)
         { 
             $table->increments('id'); 
+            $table->string('nome', 100);
             $table->string('email')->unique(); 
+            $table->string('password', 100);
+            $table->boolean('admin');
             $table->timestamps(); 
         });
 
