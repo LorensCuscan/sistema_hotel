5d8d36740f61d251de95249fe53c597cc6447f9c
Lucas Castellani <lucas100castellani@gmail.com>
2022-07-02 10:42:10 -0300

    fix

--- view/site/index.php
+++ view/site/index.php
@@ -41,22 +41,24 @@
 
 
 <section>
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 text-dark text-center fst-italic my-3">
-            <h1>Tranquilidade, elegância e conforto</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 text-dark text-center fst-italic my-3">
+        <h1>Tranquilidade, elegância e conforto</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           </div>
         </div>
-        <div class="row">
-          <div class="col-sm-4">
-          <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-
-    </section>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?= view('components/footer'); ?>
 
