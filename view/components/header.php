50bded7bdc8534960fb5c60c7bf26f3f06d8926b
LorensCuscan <lorenscuscan@gmail.com>
2022-07-03 04:38:43 -0400

    Mudanças nos cards

--- view/components/header.php
+++ view/components/header.php
@@ -14,18 +14,18 @@
   <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/sistema_hotel/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="admin">login</a>
-          </li>
-          <li class="nav-item dropdown">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/sistema_hotel/">Home</a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="admin">login</a>
+            </li>
+            <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Dropdown
             </a>
@@ -35,7 +35,7 @@
               <li><hr class="dropdown-divider"/></li>
               <li><a class="dropdown-item" href="#">Something else here</a></li>
             </ul>
-          </li>
+            </li>
           <li class="nav-item">
             <a class="nav-link disabled">Disabled</a>
           </li>
--- view/site/index.php
+++ view/site/index.php
@@ -1,90 +1,100 @@
 <?= view('components/header'); ?>
 
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+<br>
 
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
 
+
+  </div>
   <div class="carousel-inner">
-
-    <div class="carousel-item active ">
-      <img src="https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2018/10/19/1052/Grand-Hyatt-Sao-Paulo-P843-Grand-Twin-Bed-Room.jpg/Grand-Hyatt-Sao-Paulo-P843-Grand-Twin-Bed-Room.16x9.jpg?imwidth=1280" style="width:100vw; max-height:90vh;" class="d-block mh-75 h-75" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Quartos Simples...</h5>
-        <p>Pensados em cada detalhe pra voce!</p>
+    <div class="carousel-item active">
+      <img src="https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2014/09/21/1420/KOLKA-P030-Guchhi-Evening.jpg/KOLKA-P030-Guchhi-Evening.16x9.jpg?imwidth=1280" style="width:100vw; max-height:90vh;" alt="...">
+      <div class="carousel-caption d-none d-md-block ">
+      <article>
+          <header>
+            <h1>Venha comemorar conosco</h1>
+            <p>Um salão de festas exclusivo para suas data mais especiais!</p>
+      </article>
       </div>
-    </div>
-
-    <div class="carousel-item">
-      <img src="https://www.oitihotel.com.br/wp-content/uploads/2021/10/04.jpeg" style="width:100vw; max-height:90vh;" class="d-block" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Um Hotel com Spa</h5>
-        <p>Para tornar a sua experiencia única.</p>
-      </div>
-    </div>
-
-    <div class="carousel-item">
-      <img src="https://pizarromoveis.com.br/wp-content/uploads/2019/08/quartos-de-hotel-feitos-com-moveis-planejados-1280x720.png"  style="width:100vw; max-height:90vh;" class=" d-block" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Bem pertinho</h5>
-        <p>o hotel Atrium fica há apenas 5 minutos da praia.</p>
-      </div>
-    </div>
-
   </div>
-  
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"   data-bs-slide="prev">
+    <div class="carousel-item">
+      <img src="https://blog.chatuba.com.br/wp-content/uploads/2019/07/2019-07-19-como-organizar-o-banheiro-1280x720.jpg" style="width:100vw; max-height:90vh;" class="d-block" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <article>
+          <header>
+            <h1>Para tornar a sua experiencia única.</h1>
+            <p>Atenção a cada detalhe!</P>
+          </article>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://cf.bstatic.com/images/hotel/max1280x900/276/276667969.jpg"  style="width:100vw; max-height:90vh;" class=" d-block" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      <article>
+          <header>
+            <h1>Todo charme e conforto que você merece</h1>
+            <p>Confira!</P>
+          </article>
+      </div>
+    </div>
+  </div>
+</div>
+<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+</button>
 
-</div>
+<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Next</span>
+</button>
+<br>
+<br>
 
-
-
-<section>
-<div class="container my-5">
-  <div class="row">
-    <div class="col-12 col-md-4">
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="card h-25">
+        <img src="https://hoteldom.com.br/wp-content/uploads/2021/08/Hero-01-1.jpg" class="card-img-top" alt="...">
+        <div class="card-body mb-3">
+          <h5 class="card-title fst-italic">Tranquilidade</h5>
+          <p class="card-text fst-italic">Uma experiencia única</p>
+        </div>
+          <div class="card-footer">
         </div>
       </div>
     </div>
-    <div class="col-12 col-md-4">
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
+
+    <div class="col">
+  <div class="card h-100">
+    <img src="https://hoteldom.com.br/wp-content/uploads/2021/08/Hero-02.jpg" class="card-img-top" alt="...">
+      <div class="card-body card text-bg-dark  mb-3">
+        <h5 class="card-title fst-italic">O bistrô Atrium</h5>
+          <p class="card-text fst-italic">Desfrute de todo sabor </p>
+      </div>
+        <div class="card-footer">
       </div>
     </div>
-    <div class="col-12 col-md-4">
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
+  </div>
+  
+  <div class="col">
+    <div class="card h-25">
+      <img src="https://hoteldom.com.br/wp-content/uploads/2021/08/hero-03.jpg" class="card-img-top" alt="...">
+      <div class="card-body card text-bg-success mb-3">
+        <h5 class="card-title fst-italic">Todo conforto</h5>
+        <p class="card-text fst-italic">que voce precisar em qualquer lugar</p>
+      </div>
+      <div class="card-footer">
       </div>
     </div>
   </div>
 </div>
-</section>
+              
+            
+
 
 <?= view('components/footer'); ?>
 
