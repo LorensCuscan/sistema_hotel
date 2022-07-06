e8be0616e31f50b68245de2f73c7565bfcf8c6bb
LorensCuscan <lorenscuscan@gmail.com>
2022-07-06 11:33:15 -0400

    validação email fix

--- app/Controller/LoginController.php
+++ app/Controller/LoginController.php
@@ -7,5 +7,13 @@ class LoginController
     public function login()
     {
         echo $_POST["email"];
+        echo $_POST["password"];
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo("$email é um email valido");
+          } else {
+            echo("$email não é um email valido!");
+          }
+          
     }
 }
