19a4f88faa9ee8ecbe3ef129a90fb0f938f4573d
LorensCuscan <lorenscuscan@gmail.com>
2022-07-08 17:20:29 -0400

    quartos adicionados

--- view/admin/rooms.php
+++ view/admin/rooms.php
@@ -2,4 +2,57 @@
 
 
 
+<div class="card" style="width: 30rem; margin-left: 0">
+    <img src="https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2014/09/21/1735/INCHE-P166-Grand-Deluxe-Suite-King-Bedroom.jpg/INCHE-P166-Grand-Deluxe-Suite-King-Bedroom.16x9.jpg?imwidth=1280" class="card-img-top" alt="...">
+    <div class="card-body">
+        <p class="fw-bold" >Suite Master Plus </p>
+        <p class="text-card"> 800,00$</p>
+    </div>
+</div>
+
+<div class="card" style="width: 30rem;">
+    <img src="https://www.motelprive.com/wp-content/uploads/2014/03/standard-01.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+        <p class="fw-bold">Suite Standard </p>
+        <p class="card-text"> 500,00$
+    </div>
+</div>
+
+
+<div class="card"  style="width: 30rem; margin-left: 30rem; margin-top: -50rem;">
+    <img src="https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2016/05/16/1143/Hyatt-Regency-San-Francisco-Airport-P024-Business-Suite-Bedroom.jpg/Hyatt-Regency-San-Francisco-Airport-P024-Business-Suite-Bedroom.16x9.jpg?imwidth=1280" class="card-img-top" alt="...">
+    <div class="card-body">
+        <p class="fw-bold">Suite Business</p>
+        <p class="card-text"> 350,00$</p>
+    </div>
+</div>
+
+<div class="card" style="width: 30rem; margin-left:30rem; margin-top: 13px;">
+  <img src="https://www.guiademoteis.com.br/Images/moteis/94-Cassino-Motel/suites/9582-Black-Jack/fotos/2169ceb8-6eee-4499-8afa-64afaf180073-suites.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="fw-bold">Suite Black</p>
+    <p class="card-text">400,00$</p>
+  </div>
+</div>
+
+<div class="card" style="width: 30rem; margin-left:60rem; margin-top: -50rem;">
+  <img src="https://casacor.abril.com.br/wp-content/uploads/sites/7/2017/05/suc3adterio_-lio-simas_web.jpg?quality=90&strip=info" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="fw-bold">Suite rêtro</p>
+    <p class="card-text"> 600,00$</p>
+  </div>
+</div>
+
+<div class="card" style="width: 30rem; margin-left: 60rem;">
+  <img src="https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2018/10/15/1451/Grand-Hyatt-Sao-Paolo-P797-Diplomatic-Suite.jpg/Grand-Hyatt-Sao-Paolo-P797-Diplomatic-Suite.16x9.jpg?imwidth=1280" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="fw-bold">Suite Deluxe</p>
+    <p class="card-text">700,00$</p>
+  </div>
+</div>
+
+
+    
+
+
 <?= view('components/footer'); ?>
