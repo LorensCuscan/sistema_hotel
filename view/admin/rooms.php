9dc3ba5e5e8f856fd4a635872ad987f785820411
LorensCuscan <lorenscuscan@gmail.com>
2022-07-09 16:08:52 -0400

    fix

--- view/admin/rooms.php
+++ view/admin/rooms.php
@@ -3,7 +3,6 @@
 
 <form method="post">
 
-
 <form>
 <div class="form-group">
     <label>Nome do quarto</label>
@@ -17,22 +16,13 @@
 
 <div class="form-group">
     <label>Classe</label>
-    <input type="text" class="form control" name="classe">
+    <input type="number" min="1" max="5" value="1" class="form control" name="classe">
 </div>
 
 <section>
     <button type="submit" class="btn btn-success"> cadastrar quarto </button>
 </section> 
 </form>
-
-
-
-
-
-
-
-
-    
 
 
 <?= view('components/footer'); ?>
