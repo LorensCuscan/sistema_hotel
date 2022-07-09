faa4406fed1e2b54a858c8caa2908d086828eb47
Lucas Castellani <lucas100castellani@gmail.com>
2022-07-09 15:43:42 -0300

    hosts configurado

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 // constantes do projeto
 const APP_NAME = "sistema_hotel";
-const URL_BASE = "http://www.localhost/sistema_hotel";
+const URL_BASE = "http://www.atrium.local/";
 
 // constantes do banco
 const DB_DRIVER = "mysql";
--- public/index.php
+++ public/index.php
@@ -2,17 +2,17 @@
     
 session_start();
 
-require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 
 // Arquivo de constantes para sabermos os dados do projeto
-require_once __DIR__ . "/config.php";
+require_once __DIR__ . "/../config.php";
 
 // Arquivo com funções que podem nos ajudar durante o desenvolvimento
-require_once __DIR__ . "/config/helpers.php";
+require_once __DIR__ . "/../config/helpers.php";
 
 // Arquivo que faz a conexão com o banco de dados
-require_once __DIR__ . "/config/database.php";
+require_once __DIR__ . "/../config/database.php";
 
 // Arquivo de rotas
-require_once __DIR__ . "/routes.php";
+require_once __DIR__ . "/../routes.php";
 
