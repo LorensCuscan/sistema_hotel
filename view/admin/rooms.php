cd0b894d3d18c7d11b298cf69d6f1ff9a753fed5
LorensCuscan <lorenscuscan@gmail.com>
2022-07-10 03:52:01 -0400

    desenvolvimento sistema de cadastro de quarto

--- view/admin/rooms.php
+++ view/admin/rooms.php
@@ -1,13 +1,15 @@
 <?= view('components/header'); ?>
 
 
-<form method="post">
 
-<form>
-<div class="form-group">
-    <label>Nome do quarto</label>
-    <input type="text" class="form control" name="nome">
-</div>
+
+
+<form method="post" action="" method="post">
+    <form>
+        <div class="form-group">
+            <label>Nome do quarto</label>
+            <input type="text" class="form control" name="nome">
+        </div>
 
 <div class="form-group">
     <label>Preço</label>
@@ -19,9 +21,41 @@
     <input type="number" min="1" max="5" value="1" class="form control" name="classe">
 </div>
 
-<section>
-    <button type="submit" class="btn btn-success"> cadastrar quarto </button>
+
+<script>
+    $('#cadastro').on('click', function(){
+        var id = $('#id').val()
+        var name = $('#name').val()
+        var price = $( 'price').val()
+        var class = $('class').val()
+        $.ajax({
+            url: "rooms", 
+            dataType: "json",
+            type: "POST",
+            data: {
+                id: id
+                name: name
+                price: price
+                class: classe
+            },
+            success: function (res){
+                if(res == 'success'){
+                    return window.location.href = "rooms";
+                    <?php  exibirAlerta("success"); ?>
+                }
+                console.log(res)
+                alert(res)
+            }
+        }
+        )
+    })
+    <section>
+    <div>
+        <button type="submit" class="btn btn-success"> cadastrar quarto </button>
 </section> 
+</script>
+
+
 </form>
 
 
