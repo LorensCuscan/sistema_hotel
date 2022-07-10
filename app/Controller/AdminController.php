367723c494758d19cb7d7df0d04ab143e4ab5b5b
LorensCuscan <lorenscuscan@gmail.com>
2022-07-10 03:52:15 -0400

    fiz-cadastro

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -7,11 +7,60 @@ class AdminController extends Controller
     public function index()
     {
         $rooms = $this->router->route("painel.rooms");
-        return view('admin/index', compact("rooms"));
-    }
-    
-    public function listRooms()
-    {
-        return view('admin/rooms');
+        return view('admin/rooms', compact("rooms"));
     }
 }
+
+namespace view\admin\rooms;
+class RegisterController extends Controller
+{
+    public function Cadastro()
+    {
+        // Atribui os valores de Id, name, price, class
+        $id = $_POST["id"];
+        $name = $_POST["name"];
+        $price = $_POST["price"];
+        $class = $__POST["class"];
+       
+        // Caso passe por todas as verificações, atribuímos
+        // os dados do usuário na superglobal $_session
+        $_SESSION['id']  = $user['id'];
+        $_SESSION['name'] = $user['name'];
+        $_SESSION['price'] = $user['price'];
+        $_SESSION['class']    = session_id();
+
+        // retorna uma string success para o ajax
+        echo json_encode("success");
+        return view("view/admin/rooms");
+        exit;
+    }
+}
+namespace App\Controller;
+
+use view\admin\rooms;
+use CoffeeCode\Router\Router;
+
+class RegisterController extends Controller
+{
+    public function Cadastro()
+    {
+        // Atribui os valores de Id, name, price, class
+        $id = $_POST["id"];
+        $name = $_POST["name"];
+        $price = $_POST["price"];
+        $class = $__POST["class"];
+       
+        // Caso passe por todas as verificações, atribuímos
+        // os dados do usuário na superglobal $_session
+        $_SESSION['id']  = $user['id'];
+        $_SESSION['name'] = $user['name'];
+        $_SESSION['price'] = $user['price'];
+        $_SESSION['class']    = session_id();
+
+        // retorna uma string success para o ajax
+        echo json_encode("success");
+        return view("view/admin/rooms");
+        exit;
+    }
+}
+
