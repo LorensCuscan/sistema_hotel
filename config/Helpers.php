2043aef17dd3f57f570e299476784752d84303dd
Lucas Castellani <lucas100castellani@gmail.com>
2022-07-12 12:28:05 -0300

    botao de logout/ variavel global router

--- config/Helpers.php
+++ config/Helpers.php
@@ -4,8 +4,8 @@
 if(!function_exists('view')){
     function view($view, $variable = false)
     {
+        $router = $GLOBALS['router'];
         $caminhoArquivo = $_SERVER['DOCUMENT_ROOT'] . '/../view/' . $view . '.php';
-    
         if (file_exists($caminhoArquivo)) {
             if (is_array($variable)) {
                 extract($variable);
--- view/components/header.php
+++ view/components/header.php
@@ -4,15 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Atrium Hotel</title>
 
-    <!--jquery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-</head>
+
+  </head>
 <body style="background-color: #dadada;">
   <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
     <div class="container-fluid">
@@ -41,6 +39,11 @@
             </li>
             <li class="nav-item">
               <a class="nav-link active fst-italic" href="admin">Administrativo</a>
+            </li> 
+            <li class="nav-item">
+              <a class="nav-link active fst-italic" href="
+                <?= isset($_SESSION['name']) ? $router->route('logout') : $router->route('login') ?>
+                "><?= isset($_SESSION['name']) ? 'Logout' : 'Login' ?></a>
             </li>
         </ul>
       </div>
